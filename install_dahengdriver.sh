d0a0022913788a793703a068b0a6141dec878871
beniroquai <bene.d@gmx.de>
2024-08-20 10:39:39 +0200

    Update errors that cause problems on the pi

--- install_dahengdriver.sh
+++ install_dahengdriver.sh
@@ -3,6 +3,9 @@
 # Create the udev rules directory
 mkdir -p /etc/udev/rules.d
 
+sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
+
+
 # Download and install the appropriate Hik driver based on architecture
 cd /tmp
 wget https://dahengimaging.com/downloads/Galaxy_Linux_Python_2.0.2106.9041.tar_1.gz
@@ -20,7 +23,7 @@ python3 setup.py build
 python3 setup.py install
 
 # Run the installer script using expect to automate Enter key presses
-echo "Y En Y" | /tmp/Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202/Galaxy_camera.run
+echo "Y En Y" | sudo /tmp/Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202/Galaxy_camera.run
 
 # Set the library path
 export LD_LIBRARY_PATH="/usr/lib:/tmp/Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202:$LD_LIBRARY_PATH"
--- install_hikdriver.sh
+++ install_hikdriver.sh
@@ -3,6 +3,7 @@
 cd /tmp 
 wget https://www.hikrobotics.com/cn2/source/support/software/MVS_STD_GML_V2.1.2_231116.zip 
 unzip MVS_STD_GML_V2.1.2_231116.zip 
+echo "Install Hik Driver"
 sudo dpkg -i MVS-2.1.2_aarch64_20231116.deb
 cd /opt/MVS/Samples/aarch64/Python/
 cp GrabImage/GrabImage.py MvImport/GrabImage.py
