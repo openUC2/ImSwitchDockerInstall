227a7d4052cf9c9c3ad1d60dc3d31cb1c259b330
beniroquai <bene.d@gmx.de>
2024-08-20 08:37:50 +0200

    Add install scripts for cameras

--- install_all.sh
+++ install_all.sh
@@ -12,6 +12,14 @@ echo "Clone ImSwitchConfig"
 chmod +x git_clone_imswitchconfig.sh
 ./git_clone_imswitchconfig.sh
 
+echo "Install HIK Driver"
+chmod +x install_hikdriver.sh
+./install_hikdriver.sh
+
+echo "Install Daheng Driver"
+chmod +x install_dahengdriver.sh
+./install_dahengdriver.sh
+
 echo "Set Autostart for ImSwitch"
 chmod +x setup_autostart.sh
 ./setup_autostart.sh
---
+++ install_dahengdriver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Create the udev rules directory
+mkdir -p /etc/udev/rules.d
+
+# Download and install the appropriate Hik driver based on architecture
+cd /tmp
+wget https://dahengimaging.com/downloads/Galaxy_Linux_Python_2.0.2106.9041.tar_1.gz
+wget https://dahengimaging.com/downloads/Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202.zip
+unzip Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202.zip
+tar -zxvf Galaxy_Linux_Python_2.0.2106.9041.tar_1.gz
+
+# Set permissions and install the Galaxy camera driver
+cd Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202
+chmod +x Galaxy_camera.run
+
+# Build and install the Python API
+cd /tmp/Galaxy_Linux_Python_2.0.2106.9041/api
+python3 setup.py build
+python3 setup.py install
+
+# Run the installer script using expect to automate Enter key presses
+echo "Y En Y" | /tmp/Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202/Galaxy_camera.run
+
+# Set the library path
+export LD_LIBRARY_PATH="/usr/lib:/tmp/Galaxy_Linux-armhf_Gige-U3_32bits-64bits_1.5.2303.9202:$LD_LIBRARY_PATH"
+
+# Install Python packages
+pip3 install pillow numpy
+
+# Source the bashrc file
+echo "source ~/.bashrc" >> ~/.bashrc
+source ~/.bashrc
+
