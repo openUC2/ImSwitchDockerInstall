6b01cab0bbefd44e87b23a2c71d88a1223915476
beniroquai <bene.d@gmx.de>
2024-09-23 13:26:40 +0200

    Update create_desktopicons.sh

--- create_desktopicons.sh
+++ create_desktopicons.sh
@@ -15,17 +15,17 @@ chmod +x "$DESKTOP_PATH/update_docker_container.sh"
 echo "#!/bin/bash
 #!/bin/bash
 # Run the docker container with specified parameters
-sudo docker run -it --rm -p 8001:8001 -p 2222:22
--e CONFIG_PATH=/config
--e DATA_PATH=/dataset
--v ~/Documents/imswitch_docker/imswitch_git:/tmp/ImSwitch-changes
--v ~/Documents/imswitch_docker/imswitch_pip:/persistent_pip_packages
--v ~/Downloads:/dataset
--v ~/:/config
--e HEADLESS=1
--e HTTP_PORT=8001
--e UPDATE_INSTALL_GIT=0
--e UPDATE_CONFIG=0
+sudo docker run -it --rm -p 8001:8001 -p 2222:22 \
+-e CONFIG_PATH=/config \
+-e DATA_PATH=/dataset \
+-v ~/Documents/imswitch_docker/imswitch_git:/tmp/ImSwitch-changes \
+-v ~/Documents/imswitch_docker/imswitch_pip:/persistent_pip_packages \
+-v ~/Downloads:/dataset \
+-v ~/:/config \
+-e HEADLESS=1 \
+-e HTTP_PORT=8001 \
+-e UPDATE_INSTALL_GIT=0 \
+-e UPDATE_CONFIG=0 \
 --privileged ghcr.io/openuc2/imswitch-noqt-x64:latest
 " > "$DESKTOP_PATH/launch_docker_container.sh"
 
