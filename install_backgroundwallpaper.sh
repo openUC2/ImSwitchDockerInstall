fed2a10495196ee78df759aa0b3117c838b185bc
beniroquai <bene.d@gmx.de>
2024-10-13 16:47:26 +0200

    Update install_backgroundwallpaper.sh

--- install_backgroundwallpaper.sh
+++ install_backgroundwallpaper.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Install feh
-sudo apt-get install -y feh
-
 # image path
-IMAGE_PATH="./IMAGES/uc2_4k.png"
+IMAGE_PATH="./IMAGES/uc2_4k.png"  # Provide the correct absolute path to your image
 
-# check if the file exists
-if [ ! -f "$IMAGE_PATH" ]; then
-    echo "File $IMAGE_PATH not found."
-    exit 1
-fi
+# Install wallpaper directory
+WALLPAPER_ROOT="/usr/share/uc2-wallpaper"
+sudo install -d "${WALLPAPER_ROOT}"
 
-# path to the image
-ABSOLUTE_IMAGE_PATH=$(cd "$(dirname "$IMAGE_PATH")"; pwd)/$(basename "$IMAGE_PATH")
+# Install default wallpaper
+WALLPAPER_PATH="${WALLPAPER_ROOT}/uc2_4k.png"
+sudo install -v -m 644 "$IMAGE_PATH" "${WALLPAPER_PATH}"
 
-# set the desktop background
-feh --bg-scale "$ABSOLUTE_IMAGE_PATH"
+# Set default wallpaper
+TARGET_KEY="wallpaper"
+CONFIG_FILE_0="/etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf"
+CONFIG_FILE_1="/etc/xdg/pcmanfm/LXDE-pi/desktop-items-1.conf"
 
-echo "Desktop-Hintergrund was set to $ABSOLUTE_IMAGE_PATH."
+sudo sed -i "/${TARGET_KEY}=/ s|=.*|=${WALLPAPER_PATH}|" "$CONFIG_FILE_0"
+sudo sed -i "/${TARGET_KEY}=/ s|=.*|=${WALLPAPER_PATH}|" "$CONFIG_FILE_1"
+
+echo "Desktop wallpaper has been set to ${WALLPAPER_PATH}."
