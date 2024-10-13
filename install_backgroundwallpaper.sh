b4ad0b07ac1a23da2c5c2e13242d5aa0ffc1632c
beniroquai <bene.d@gmx.de>
2024-10-13 13:25:41 +0200

    Update install_backgroundwallpaper.sh

--- install_backgroundwallpaper.sh
+++ install_backgroundwallpaper.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Pfad zum Bild
-IMAGE_PATH="./IMAGES/uc2_4.png"
+# Install feh
+sudo apt-get install -y feh
 
-# Überprüfen, ob die Bilddatei existiert
+# image path
+IMAGE_PATH="./IMAGES/uc2_4k.png"
+
+# check if the file exists
 if [ ! -f "$IMAGE_PATH" ]; then
     echo "File $IMAGE_PATH not found."
     exit 1
 fi
 
-# Absoluten Pfad zum Bild erhalten
+# path to the image
 ABSOLUTE_IMAGE_PATH=$(cd "$(dirname "$IMAGE_PATH")"; pwd)/$(basename "$IMAGE_PATH")
 
-# AppleScript-Befehl zum Setzen des Desktop-Hintergrunds
-osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$ABSOLUTE_IMAGE_PATH\""
+# set the desktop background
+feh --bg-scale "$ABSOLUTE_IMAGE_PATH"
 
-echo "Desktop-Background was set to $ABSOLUTE_IMAGE_PATH."
+echo "Desktop-Hintergrund was set to $ABSOLUTE_IMAGE_PATH."
