634573614364bb285e6f98e27f85e3e0be7a489e
beniroquai <bene.d@gmx.de>
2025-02-21 09:52:39 +0100

    adjust

--- install_all_pigen.sh
+++ install_all_pigen.sh
@@ -2,7 +2,6 @@
 
 # install requirements 
 sudo apt-get update
-sudo apt-get install -y git curl
 
 # in case they don't exist create Download/Desktop folder (e.g. lite)
 mkdir ~/Downloads
@@ -12,10 +11,6 @@ cd ~/Downloads/ImSwitchDockerInstall
 echo "Install Docker"
 chmod +x install_docker_raspi.sh
 ./install_docker_raspi.sh
-
-echo "Pull and Install Docker Image"
-chmod +x pull_and_run.sh
-./pull_and_run.sh
 
 echo "Clone ImSwitchConfig"
 chmod +x git_clone_imswitchconfig.sh
@@ -29,28 +24,11 @@ echo "Install Daheng Driver"
 chmod +x install_dahengdriver.sh
 ./install_dahengdriver.sh
 
-echo "Install Vimba Driver"
-chmod +x install_vimba.sh
-./install_vimba.sh
-
-echo "Setup RaspAp"
-chmod +x install_raspap.sh
-./install_raspap.sh
-
 echo "Create Desktop Icons"
 chmod +x create_desktopicons.sh
 ./create_desktopicons.sh
 
-echo "Set Wallpaper"
-chmod +x install_backgroundwallpaper.sh
-./install_backgroundwallpaper.sh
-
 echo "Set install_autostart for ImSwitch"
 chmod +x setup_install_autostart.sh
 ./setup_install_autostart.sh
-
-# add serial devices to user group
-sudo usermod -a -G dialout $USER
-sudo usermod -a -G tty $USER
-echo "Please reboot to take effect of adding serial devices to user group"
  
