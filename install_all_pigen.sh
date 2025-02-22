3b4d25223b5c3d7b794ce32a623b49296656de3d
beniroquai <bene.d@gmx.de>
2025-02-22 13:18:13 +0100

    Update install_all_pigen.sh

--- install_all_pigen.sh
+++ install_all_pigen.sh
@@ -16,19 +16,3 @@ echo "Clone ImSwitchConfig"
 chmod +x git_clone_imswitchconfig.sh
 ./git_clone_imswitchconfig.sh
 
-echo "Install HIK Driver"
-chmod +x install_hikdriver.sh
-./install_hikdriver.sh
-
-echo "Install Daheng Driver"
-chmod +x install_dahengdriver.sh
-./install_dahengdriver.sh
-
-echo "Create Desktop Icons"
-chmod +x create_desktopicons.sh
-./create_desktopicons.sh
-
-#echo "Set install_autostart for ImSwitch"
-#chmod +x install_autostart.sh
-#./install_autostart.sh
- 
